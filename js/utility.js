59e1334a3dd1c918a71a5d21d0d43b3d1ea27d5f
nuhaiyan <hossainmdtarekhossain3@gmail.com>
2024-02-18 19:56:23 +0600

    pop up button done

--- js/utility.js
+++ js/utility.js
@@ -1,1 +1,12 @@
-console.log("connected");
+
+const btnNext = document.getElementById('btn-next');
+const screenBlack = document.getElementById('screenBlack');
+const popUp = document.getElementById('popUp');
+
+
+// next button pop up
+btnNext.addEventListener('click', function() {
+    screenBlack.classList.remove('hidden');
+    popUp.classList.remove('hidden');
+});
+
